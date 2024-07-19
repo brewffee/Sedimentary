bdb69de29b26a1d9d744622a2532064276fb9c36
brewffee <geixco@pm.me>
2024-07-19 18:06:38 -0400

    New shorthand for creating blocks and block entities

--- src/main/java/coffee/ktty/sedimentary/util/Shorthand.java
+++ src/main/java/coffee/ktty/sedimentary/util/Shorthand.java
@@ -1,8 +1,18 @@
 package coffee.ktty.sedimentary.util;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.ToIntFunction;
 
 import static coffee.ktty.sedimentary.Sedimentary.ID;
 
@@ -19,5 +29,47 @@ public final class Shorthand {
     @Contract("_ -> new")
     public static @NotNull Identifier id(String path) {
         return new Identifier(ID, path);
+    }
+
+    /**
+     * Creates a new {@link Block} by copying an existing Block's settings
+     *
+     * @param block the {@link Block} to copy from
+     * @return the block settings object
+     */
+    @Contract(value = "_ -> new", pure = true)
+    public static AbstractBlock.@NotNull Settings copyFrom(Block block) {
+        return FabricBlockSettings.copy(block);
+    }
+
+    /**
+     * Creates a new {@link Block} from scratch
+     *
+     * @return the block settings object
+     */
+    @Contract(value = " -> new", pure = true)
+    public static AbstractBlock.@NotNull Settings create() {
+        return FabricBlockSettings.create();
+    }
+
+    /**
+     * Creates a new {@link BlockEntity} from scratch
+     *
+     * @return the block entity type
+     */
+    @Contract("_, _ -> new")
+    public static <T extends BlockEntity> BlockEntityType<? extends T> createEntity(FabricBlockEntityTypeBuilder.Factory<? extends T> factory, Block... blocks) {
+        return FabricBlockEntityTypeBuilder.create(factory, blocks).build(null);
+    }
+
+    /**
+     * Quick function to define a block's light level in its LIT state.
+     *
+     * @param level int
+     * @return the light level
+     */
+    @Contract(pure = true)
+    public static @NotNull ToIntFunction<BlockState> inLitState(int level) {
+        return Blocks.createLightLevelFromLitBlockState(level);
     }
 }
